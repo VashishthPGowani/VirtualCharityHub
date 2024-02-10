da5731a7a21e481d5505a555d1e8c90c95b420c1
Sahil Gajera <sahilsgajera@gmail.com>
2024-02-09 23:42:43 -0500

    admin All component design changes

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+    registerForm: FormGroup;
+  
+    constructor(private formBuilder: FormBuilder) { }
+  
+    ngOnInit(): void {
+      this.registerForm = this.formBuilder.group({
+        name: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        phone: [''],
+        password: ['', Validators.required],
+      });
+    }
+  
+    // convenience getter for easy access to form fields
+    get f() { return this.registerForm.controls; }
+  
+    onSubmit() {
+      // do something with the form data
+      console.log(this.registerForm.value);
+    }
   }
-
-}
