998666249aa18bbc4ed73d11e3a1279668f07359
Vashishth <govaniv8866@gmail.com>
2024-03-27 11:39:06 -0400

    charity and Campaign list dynamic

--- src/app/campaign-list/campaign-list.component.ts
+++ src/app/campaign-list/campaign-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import Swal from 'sweetalert2';
+import { CampaignService } from '../services/campaign.service';
 
 @Component({
   selector: 'app-campaign-list',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./campaign-list.component.css']
 })
 export class CampaignListComponent implements OnInit {
+  campaigns:any;
+  status = 1;
+  imagePath:string;
 
-  constructor() { }
+  constructor(private campaignService:CampaignService) { }
 
   ngOnInit(): void {
+    this.imagePath = environment.apiUrl + '/uploads/images/';
+    this.GetCampaigns();
+  }
+
+  GetCampaigns()
+  {
+    this.campaignService.getCampaigns(this.status).subscribe({
+      next: (response:any) => {
+       this.campaigns = response;
+      },
+      error: (error) => {
+      Swal.fire('error','internal error','error');
+      },
+      complete: () => {
+     
+      }
+    })
   }
 
 }
--- src/app/charity-list/charity-list.component.ts
+++ src/app/charity-list/charity-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import Swal from 'sweetalert2';
+import { CharityService } from '../services/charity.service';
 
 
 @Component({
@@ -7,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./charity-list.component.css']
 })
 export class CharityListComponent implements OnInit {
+  charities:any;
+  imagePath:string;
+  status = 1;
 
-  constructor() { }
+  constructor(private charityService:CharityService) { }
 
   ngOnInit(): void {
+    this.imagePath = environment.apiUrl + '/uploads/images/';
+    this.GetCharities();
+  }
+
+  GetCharities()
+  {
+    this.charityService.getCharities(this.status).subscribe({
+      next: (response:any) => {
+       this.charities = response;
+      },
+      error: (error) => {
+      Swal.fire('error','internal error','error');
+      },
+      complete: () => {
+     
+      }
+    })
   }
 
 }
